7e5e0e19155ad09a720e8b71d44f14af6e7ed4a6
Zaccaria Al-Fatlawi <z.alfatlawi@icloud.com>
2024-02-15 17:35:27 +0100

    minor changes

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -11,19 +11,17 @@ enum VisitorAction {
 #[derive(Debug)]
 struct Visitor {
     name: String,
-    greeting: String,
+    action: VisitorAction,
+    age: i8
 }
 
 impl Visitor {
-    fn new(name: &str, greeting: &str) -> Self {
+    fn new(name: &str, action: VisitorAction, age: i8) -> Self {
         Self {
             name: name.to_lowercase(),
-            greeting: greeting.to_string(),
+            action,
+            age
         }
-    }
-
-    fn greet_visitor(&self) {
-        println!("{}", self.greeting);
     }
 }
 
@@ -37,9 +35,11 @@ fn what_is_your_name() -> String {
 
 fn main() {
     let mut visitor_list = vec![
-        Visitor::new("bert", "Hello Bert, enjoy your treehouse"),
-        Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
-        Visitor::new("fred", "Wow, who invited Fred?"),
+        Visitor::new("Bert", VisitorAction::Accept, 45),
+        Visitor::new("Steve", VisitorAction::AcceptWithNote {
+            note: String::from("Lactose-free milk is in the fridge") 
+        }, 15),
+        Visitor::new("Fred", VisitorAction::Refuse, 30),
     ];
     loop {
         println!("Hello, what's your name? (Leave empty and press ENTER to quit)");
